a7985addefe89330a95ce3dac62c8f2b2f981549
keshav <keshav.kishore00000@gmail.com>
2019-01-11 00:28:43 +0530

    changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    
 # Create your views here.
